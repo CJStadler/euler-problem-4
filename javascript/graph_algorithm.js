c82f35c0888ee9680c7dcea281563522fd0b1dca
Chris Stadler <chrisstadler@gmail.com>
2017-11-25 21:28:07 -0500

    WIP number lines

--- javascript/graph_algorithm.js
+++ javascript/graph_algorithm.js
@@ -16,15 +16,38 @@ function graph_algorithm(n, algorithm_function, container_selector) {
 }
 
 function init_graph(container_selector, n) {
-  var margin = {top: 20, right: 20, bottom: 20, left: 35},
-    width = 500 - margin.left - margin.right,
-    height = 340 - margin.top - margin.bottom;
+  var width = 500;
 
   var svg = d3.select(container_selector).append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom);
+    .attr("width", width);
+
+  init_first_factor_column(svg, n);
+  init_second_factor_column(svg, n);
 
   return svg;
+}
+
+function init_first_factor_column(svg, n) {
+  var column = svg.append('g')
+    .attr('id', 'first-column')
+    .attr('width', 100);
+
+  init_column_factors(column, n)
+}
+
+function init_column_factors(column, n) {
+  var largest = largest_with_digits(n);
+  var smallest = smallest_with_digits(n);
+
+  var n_digit_numbers = [];
+  for (i = largest; i >= smallest; i--) {
+    n_digit_numbers.push(i);
+  }
+
+  column.selectAll('.number_containers')
+    .data(n_digit_numbers)
+    .enter()
+    .append('g')
 }
 
 function graph(svg, multiplications) {
